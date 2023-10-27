34ce5dc33fa840426db8a38b152f60a819ff6bef
chenwj <731028571@qq.com>
2023-10-28 02:20:31 +0800

    feat: add hmr package

---
+++ hmr/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: './index.js',
+    mode: 'development',
+    devtool: false,
+    optimization: {
+        usedExports: false,
+        minimize: false,
+        sideEffects: false,
+        innerGraph: false,
+        providedExports: false,
+        mangleExports: false, // module的exports 保留变量名字, 还是被压缩
+        concatenateModules: false,
+    },
+    devServer: {
+        devMiddleware: {
+            writeToDisk: true
+        },
+        static: ['dist', 'public'],
+    }
+}
