9a921aa6abfb368613458882a6ff9b2f4a43d8d8
cwj0417 <chenwenjun@yo-cwj.com>
2023-12-07 16:18:21 +0800

    feat: add pkg measure

---
+++ measure/index.js
@@ -0,0 +1,5 @@
+import { add } from './add';
+import { sub } from './sub';
+
+console.log(add(1, 2))
+console.log(sub(1, 2))
---
+++ measure/webpack.config.js
@@ -0,0 +1,82 @@
+const NM = require('webpack/lib/NormalModule')
+const pluginname = 'measure'
+module.exports = {
+    entry: './index.js',
+    mode: 'development',
+    devtool: false,
+    optimization: {
+        usedExports: false,
+        minimize: false,
+        sideEffects: false,
+        innerGraph: false,
+        providedExports: false,
+        mangleExports: false,
+        concatenateModules: false,
+    },
+    module: {
+        rules: [{
+            test: /add/,
+            loader: './loader-direct.js'
+        }]
+    },
+    plugins: [{
+        apply(compiler) {
+            compiler.hooks.compilation.tap(pluginname, (compilation) => {
+
+                // utils
+
+                const formatId = (id) => id.replace(/.*\/(.*)/, '$1')
+
+                // entry
+
+                // const entryAdd = (entry, option) => {
+                //     console.log('add entry', entry.request, option.name)
+                // }
+
+                // const entryDone = (entry, option) => {
+                //     console.log('done entry', entry.request, option.name)
+                // }
+                // compilation.hooks.addEntry.tap(pluginname, entryAdd);
+                // compilation.hooks.failedEntry.tap(pluginname, entryDone);
+                // compilation.hooks.succeedEntry.tap(pluginname, entryDone);
+
+                // module
+
+                // module add and done
+
+                // compilation.addModuleQueue.hooks.added.tap(pluginname, (m) => {
+                //     console.log('add module', formatId(m.resource))
+                // });
+                // compilation.processDependenciesQueue.hooks.result.tap(pluginname, (m) => {
+                //     console.log('module done', formatId(m.resource))
+                // });
+
+                // module build q in & out
+                // compilation.buildQueue.hooks.added.tap(pluginname, (m) => {
+                //     console.log('(in queue) build', formatId(m.resource))
+                // });
+                compilation.hooks.buildModule.tap(pluginname, (m) => {
+                    console.log('build start', formatId(m.resource))
+                });
+                const moduleHooks = NM.getCompilationHooks(compilation)
+                moduleHooks.beforeLoaders.tap(pluginname, (loaders, m) => {
+                    console.log('<loader> init', formatId(m.resource), loaders.map(i => i.loader))
+                })
+                moduleHooks.beforeParse.tap(pluginname, (m) => {
+                    console.log('<loader> done [before parse]', formatId(m.resource))
+                })
+
+                moduleHooks.beforeSnapshot.tap(pluginname, (m) => {
+                    console.log('[parse and create hash] done, create snapshot', formatId(m.resource))
+                })
+
+                compilation.hooks.succeedModule.tap(pluginname, (m) => {
+                    console.log('build suc', formatId(m.resource))
+                })
+                // compilation.buildQueue.hooks.result.tap(pluginname, (m) => {
+                //     console.log('(out queue) build', formatId(m.resource))
+                // });
+            })
+        }
+    }]
+}
